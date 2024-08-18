62553fd246149e4f1114ee1e365c09b4eee6b9a7
swarnim3103 <swarnim3103@gmail.com>
2024-08-18 14:39:40 +0530

    Changes to be committed: 	new file:   src/assets/icons/clock-svgrepo-com.svg 	new file:   src/assets/icons/down-arrow-multimedia-option-svgrepo-com.svg 	new file:   src/assets/icons/light-bulb-svgrepo-com.svg 	deleted:    src/assets/react.svg 	modified:   src/pages/Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,22 +2,40 @@ import React from 'react';
 import Nav from '../sections/Nav.jsx';
 import Footer from '../sections/Footer.jsx';
 import Game from '../sections/game.jsx';
+import Typewriter from '../sections/Typewriter.jsx';
 
 const Dashboard = () => {
+  const scrollToSection = () => {
+    const section = document.getElementById('scroll-target');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <>
       <Nav />
-      <div className="flex items-center bg-gray-100 mb-4">
-        <div className="bg-white p-8 shadow-md w-full">
-          <h2 className="text-4xl font-bold mb-6 text-center text-green-500">
-            Welcome To <span className="text-green-900 underline">WasteZero</span>
+      <div className=" items-center bg-gray-100 mb-4">
+        <div className="bg-white shadow-md w-full h-screen pt-40">
+          <h2 className="text-8xl font-bold text-center text-green-500">
+            Welcome To <Typewriter text="Zero Waste Business Game" />
           </h2>
+          <p className="text-center text-2xl font-bold text-green-600">Scroll Down</p>
+          <img src="src\assets\icons\down-arrow-multimedia-option-svgrepo-com.svg" alt="Scroll Down" className="w-12 h-12 mx-auto mt-10"  onClick={scrollToSection} />
         </div>
       </div>
-      <Game />
-      <div className="flex flex-col w-[90%] gap-4 mx-auto">
-        {/* Add content here */}
+      <div id="scroll-target" className="flex w-[90%] gap-4 mx-auto">
+        <div className="text-4xl text-center">
+        <img src="src\assets\icons\light-bulb-svgrepo-com.svg" alt="Visual Elements" className=""  />
+          <h1>Score</h1>
+        </div>
+        <div>
+        <div className="text-4xl text-center">
+        <img src="src\assets\icons\clock-svgrepo-com.svg" alt="Visual Elements" className="" />
+          <h1>Rank</h1>
+        </div>
+        </div>
       </div>
+      <Game/>
       <Footer />
     </>
   );
